b9839ed0d02fb081d328daccced26d28e7c04c06
g_timb <g_timb@marvin.com>
2021-12-23 18:57:23 -0600

    Saving work

---
+++ public/js/main_script.js
@@ -0,0 +1,8 @@
+// Add our DOM elements
+const submitBtn = document.getElementById("submit");
+const successMsg = document.querySelector(".success_msg");
+
+// This is a function that displays the success message when the email button is clicked
+submitBtn.addEventListener("click", () => {
+    successMsg.style.display = "block";
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+    res.render("index", { title: "Express" });
+});
+
+router.post("/email", (req, res) => {
+    console.log(req.body.name);
+    let mailOptions = {
+        from: req.body.email,
+        to: "tim@timb.codes",
+        subject: `Client Email: ${req.body.subject}`,
+        text: `Name: ${req.body.name}; Subject: ${req.body.subject}; Email: ${req.body.email} Message: ${req.body.message}`,
+    };
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "timbcodes@gmail.com",
+            pass: "$mokeyJoe86",
+        },
+    });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(`Email sent: ${info.response}`);
+        }
+    });
+    setTimeout(() => {
+        res.redirect("/");
+    }, 2000);
 });
 
 module.exports = router;
